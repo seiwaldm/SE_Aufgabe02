4f6ea3c0b5a7ebf8191fec6f99f706a75469c5b4
seiwaldm <markus.seiwald2@sbg.ac.at>
2015-10-27 19:50:36 +0100

    Reflection erweitert

--- Reflection.java
+++ Reflection.java
@@ -1,23 +1,64 @@
 import java.io.PrintStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 public class Reflection {
 
-	StringBuilder stringBuilder = new StringBuilder();
+	StringBuilder output = new StringBuilder();
 
 	public void reconstruct(Class c, PrintStream ps) {
-		// TODO Auto-generated method stub
-		c.getModifiers();
+
+		// class modifiers
+		output.append(Modifier.toString(c.getModifiers()) + " " // TODO class,
+																// interface,
+																// abstract
+																// class...
+				+ c.getSimpleName());
+
+		if (c.getSuperclass() != null) {
+			output.append(" extends " + c.getSuperclass()); // unschoen...
+		}
+
+		// implemented interfaces
+		Class[] interfaces = c.getInterfaces();
+		if (interfaces != null) {
+			output.append(" implements ");
+			for (Class i : interfaces) {
+				output.append(i.getSimpleName() + ", ");
+			}
+			output.deleteCharAt(output.length() - 2);
+		}
+		
+		output.append("{");
+
+		// fields
+		Field[] fields = c.getDeclaredFields();
+		if (fields != null) {
+			output.append("\n\n");
+			for (Field f : fields) {
+				output.append(Modifier.toString(f.getModifiers()) + " " + f.getName() + "\n");
+			}
+		}
+
+		// constructors
+		Constructor[] constructors = c.getConstructors();
+		for (Constructor con : constructors) {
+			output.append(
+					"\n" + Modifier.toString(con.getModifiers()) + " " + c.getSimpleName() + " ");
+		}
+
+		// Methods
 		Method[] methods = c.getDeclaredMethods();
 		for (Method m : methods) {
-			System.out.println(Modifier.toString(c.getModifiers()) + " " + m.getName()+ "{");
+			output.append("\n" + Modifier.toString(c.getModifiers()) + " " + m.getName() + "(");
 
 		}
+		System.out.println(output);
 	}
 
 	public void reconstruct(String fullClassname, PrintStream ps) throws ClassNotFoundException {
-		// TODO Auto-generated method stub
 
 	}
 
