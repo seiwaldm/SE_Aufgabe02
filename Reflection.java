3b44cafdde5622b9944698caf0533144410b6a59
seiwaldm <markus.seiwald2@sbg.ac.at>
2015-10-29 23:01:23 +0100

    ...so - vieles erledigt - für ArrayList funktionierts gut... der Teufel liegt aber sicher im Detail...
    Signed-off-by: seiwaldm <markus.seiwald2@sbg.ac.at>

--- Reflection.java
+++ Reflection.java
@@ -3,81 +3,113 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.Hashtable;
+import java.util.Stack;
 
 public class Reflection {
 
-	StringBuilder output = new StringBuilder();
+	StringBuilder output1 = new StringBuilder();
+	StringBuilder output2 = new StringBuilder();
+	Hashtable<String, String> usedPackages = new Hashtable<>();
 
 	public void reconstruct(Class c, PrintStream ps) {
 
-		output.delete(0, output.length());
-
 		// package:
-		output.append("package " + c.getPackage().getName() + ";\n\n");
-
-		// TODO imports:
-		// ??????? -> get lost at compile-time
+		if (c.getPackage() != null)
+			output1.append("package " + c.getPackage().getName() + ";\n\n");
 
 		// class modifiers & name
-		output.append(Modifier.toString(c.getModifiers()) + " ");
-		if (c.isInterface() == true)
-			output.append("interface ");
+		output2.append(Modifier.toString(c.getModifiers()) + " ");
+		if (c.isInterface())
+			output2.append("interface ");
 		else
-			output.append("class ");
-		output.append(c.getSimpleName() + " ");
+			output2.append("class ");
+		output2.append(c.getSimpleName() + " ");
 
 		// extended abstract class
-		if (c.getSuperclass() != null) {
-			if (Modifier.isAbstract(c.getSuperclass().getModifiers())) {
-				output.append("extends " + c.getSuperclass().getSimpleName() + " ");
-			}
+		if (Modifier.isAbstract(c.getSuperclass().getModifiers())) {
+			output2.append("extends " + c.getSuperclass().getSimpleName() + " ");
+			usedPackages(c.getSuperclass());
 		}
 
 		// implemented interfaces
 		Class[] interfaces = c.getInterfaces();
 		if (interfaces.length != 0) {
-			output.append("implements ");
+			output2.append("implements ");
 			for (Class i : interfaces) {
-				output.append(i.getSimpleName() + ", ");
+				output2.append(i.getSimpleName() + ", ");
+				usedPackages(i);
 			}
-			output.deleteCharAt(output.length() - 2);
+			output2.deleteCharAt(output2.length() - 2);
 		}
 
-		output.append("{\n\n");
+		output2.append("{\n\n");
 
 		// fields
 		Field[] fields = c.getDeclaredFields();
-		if (fields != null) {
+		if (fields.length != 0) {
 			for (Field f : fields) {
-				// TODO schaut anders aus, als in der Vorlage... :( default-Werte fehlen!
-				output.append("\t" + Modifier.toString(f.getModifiers()) + " " + f.getType().getSimpleName() + " "
-						+ f.getName() + "\n");
+				output2.append("\t" + Modifier.toString(f.getModifiers()) + " "
+						+ f.getType().getSimpleName() + " " + f.getName() + " = "
+						+ setValue(f.getType().getSimpleName()) + ";\n");
+				usedPackages(f.getType());
 			}
 		}
 
-		output.append("\n");
+		output2.append("\n");
 
 		// constructors
 		Constructor[] constructors = c.getConstructors();
 		for (Constructor con : constructors) {
-			output.append("\t" + Modifier.toString(con.getModifiers()) + " " + c.getSimpleName());
+			output2.append("\t" + Modifier.toString(con.getModifiers()) + " " + c.getSimpleName());
 			Class[] para = con.getParameterTypes();
 			printParameters(para);
+			output2.append("{\n\t\tSystem.out.println(\"constructor\");\n\t}\n\n");
 		}
 
-		output.append("\n");
+		output2.append("\n");
 
 		// methods
 		Method[] methods = c.getDeclaredMethods();
 		for (Method m : methods) {
-			output.append("\t" + Modifier.toString(c.getModifiers()) + " " + m.getReturnType().getSimpleName() + " "
-					+ m.getName());
+			output2.append("\t" + Modifier.toString(m.getModifiers()) + " "
+					+ m.getReturnType().getSimpleName() + " " + m.getName());
+			usedPackages(m.getReturnType());
 			Class[] para = m.getParameterTypes();
 			printParameters(para);
+			Class[] exceptions = m.getExceptionTypes();
+			if (exceptions.length != 0) {
+				output2.append("throws ");
+				for (Class e : exceptions) {
+					output2.append(e.getSimpleName() + ", ");
+					usedPackages(e);
+				}
+				output2.deleteCharAt(output2.length() - 2);
+			}
+			output2.append("{\n\t\tSystem.out.println(\"" + m.getName() + "\");\n\t}\n\n");
+			if (m.getReturnType() != void.class) {
+				output2.delete(output2.length() - 5, output2.length() - 1);
+				output2.append(
+						"\t\treturn " + setValue(m.getReturnType().getSimpleName()) + ";\n\t}\n\n");
+			}
 		}
 
-		output.append("\n}");
+		output2.append("}");
 
+		// imports:
+		Stack<String> imported = new Stack<String>();
+		imported.addAll(usedPackages.values());
+		while (!imported.isEmpty()) {
+			String usedPackage = imported.pop();
+			if (!usedPackage.contains(c.getPackage().getName()) && !usedPackage.contains("void")
+					&& !usedPackage.contains("java.lang"))
+				output1.append("import " + usedPackage + ";\n");
+		}
+
+		output1.append("\n");
+
+		// merge outputs:
+		String output = output1.toString() + output2.toString();
 		System.out.println(output);
 	}
 
@@ -87,15 +119,49 @@ public class Reflection {
 
 	// helper method to print parameters of constructors and methods:
 	private void printParameters(Class[] para) {
-		output.append("(");
+		output2.append("(");
 		int counter = 0;
 		for (Class p : para) {
-			output.append(p.getSimpleName() + " param" + counter + ", ");
+			output2.append(p.getSimpleName() + " param" + counter + ", ");
+			usedPackages(p);
 			counter++;
 		}
 		if (counter != 0)
-			output.delete(output.length() - 2, output.length());
-		output.append(")\n");
+			output2.delete(output2.length() - 2, output2.length());
+		output2.append(") ");
 	}
 
+	// helper method to set default value of fields and return value of methods:
+	private String setValue(String s) {
+		switch (s) {
+		case "boolean":
+			return "false";
+		case "char":
+			return "'u0000'";
+		case "double":
+			return "0.0";
+		case "float":
+			return "0.0";
+		case "int":
+			return "0";
+		case "long":
+			return "0";
+		case "short":
+			return "0";
+		case "byte":
+			return "0";
+		default:
+			return "null";
+		}
+	}
+
+	// helper method to store packages of used classes:
+	private void usedPackages(Class c) {
+		String name = c.getSimpleName();
+		if (name.charAt(name.length() - 1) == ']')
+			name = name.substring(0, name.length() - 2);
+		if (name != "boolean" && name != "char" && name != "double" && name != "float"
+				&& name != "long" && name != "short" && name != "byte" && name != "int")
+			usedPackages.put(name, c.getCanonicalName());
+	}
 }
