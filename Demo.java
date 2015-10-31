fd95da041da9fa856a50a051bf87c0b26466a3f1
schoergk <kevin@schoergnhofer.de>
2015-10-31 16:23:44 +0100

    added javadoc-comments

--- Demo.java
+++ Demo.java
@@ -14,6 +14,12 @@ import java.util.ArrayList;
 
 public class Demo {
 
+	/**
+	 * tests and shows the functionality of the Reflection class
+	 * 
+	 * @param args standard parameter
+	 *
+	 */
 	public static void main(String[] args) {
 		Reflection demo = new Reflection();
 		demo.reconstruct(ArrayList.class, null);
