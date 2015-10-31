9efb7b703c92eba7b71a24e51680b03c00c3d811
schoergk <kevin@schoergnhofer.de>
2015-10-31 15:38:04 +0100

    added javadoc-comments

--- Demo.java
+++ Demo.java
@@ -2,6 +2,16 @@ import java.util.AbstractCollection;
 import java.util.AbstractList;
 import java.util.ArrayList;
 
+/**
+ * PS Software Engineering WS2015 <br>
+ * <br>
+ * 
+ * This demo class tests and shows the functionality of the Reflection class
+ * 
+ * @author Markus Seiwald, Kevin Schoergnhofer 
+ *
+ */
+
 public class Demo {
 
 	public static void main(String[] args) {
